ea13a2d00767765aa2263776c3e2fe4e00f351e4
unknown <armestoj.michael@gmail.com>
2022-08-02 21:52:38 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const displayPreviousValue = document.getElementById('previousValue');
+const displayCurrentValue = document.getElementById('currentValue');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
